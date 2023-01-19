71e4679acae2ee461c72a889fb041eb6603ae9dd
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2023-01-19 07:40:30 -0500

    wip build expression

--- flake.nix
+++ flake.nix
@@ -2,29 +2,36 @@
   description = "Isabelle arithmetization flake";
   nixConfig.bash-prompt = "[nix-develop-isabelle:] ";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: 
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        buildInputs = with pkgs;
+          [ isabelle fontconfig util-linux
+            (texlive.combine {
+              inherit (texlive) scheme-basic collection-luatex ulem txfonts;
+            })
+          ];
+    in
+    {
 
-    devShells.x86_64-linux.default =
-      with nixpkgs.legacyPackages.x86_64-linux;
-      stdenv.mkDerivation {
-        name = "isabelle-shell";
-        buildInputs = [ isabelle fontconfig ];
-        src = ./.;
-      };
+      devShells.x86_64-linux.default =
 
-   packages.x86_64-linux.default =
-     with nixpkgs.legacyPackages.x86_64-linux;
-      stdenv.mkDerivation {
-        name = "isabelle-theories";
-        buildInputs = [ isabelle util-linux fontconfig ]
-          ++ texlive.luatex.pkgs;
-        src = ./.;
-        buildPhase = ''
-          export UUID=$(uuidgen)
-          mkdir -p /tmp/$UUID
-          HOME=/tmp/$UUID isabelle build -D .
-        '';
-      };
+        pkgs.stdenv.mkDerivation {
+          name = "isabelle-shell";
+          inherit buildInputs;
+          src = ./.;
+        };
 
-  };
+      packages.x86_64-linux.default =
+        pkgs.stdenv.mkDerivation {
+           name = "isabelle-theories";
+           inherit buildInputs;
+           src = ./theories;
+           buildPhase = ''
+             export UUID=$(uuidgen)
+             mkdir -p /tmp/$UUID
+             HOME=/tmp/$UUID isabelle build -D .
+           '';
+        };
+
+    };
 }

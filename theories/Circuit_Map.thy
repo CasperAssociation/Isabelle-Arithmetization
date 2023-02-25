c301f0204c7d90dd34992977aedbe9c171891904
Anthony Hart <soobtoob@gmail.com>
2023-02-25 05:27:57 -0800

    Implement circuit lookups

--- theories/Circuit_Map.thy
+++ theories/Circuit_Map.thy
@@ -358,6 +358,8 @@ fun map_filterWithKey :: "
   ('a \<Rightarrow> bool) \<Rightarrow> ('a, 'b) Map \<Rightarrow> ('a, 'b) Map" where
 "map_filterWithKey f = filter (f \<circ> fst)"
 
+
+
 fun getEvaluate_PolynomialVariable_Map_RowIndex_Scalar ::
   "(PolynomialVariable, (RowIndex, Scalar) Map) HasEvaluate" where
 "getEvaluate_PolynomialVariable_Map_RowIndex_Scalar arg v =
@@ -388,13 +390,57 @@ fun map_union_with :: "
   )
 "
 
+fun combineVals :: "
+  'k \<Rightarrow>
+  ('a \<Rightarrow> 'a \<Rightarrow> 'a) \<Rightarrow> 
+  ('k, 'a) Map \<Rightarrow> 
+  ('k, 'a) Map \<Rightarrow> 
+  'a" where
+"combineVals k f m1 m2 =
+  (case (map_of m1 k, map_of m2 k) of 
+    (Some x, None) \<Rightarrow> x |
+    (None, Some x) \<Rightarrow> x |
+    (Some x, Some y) \<Rightarrow> f x y
+  )"
+
 fun list_map_union_with :: "
   ('a \<Rightarrow> 'a \<Rightarrow> 'a) \<Rightarrow> 
   ('k, 'a) Map \<Rightarrow> 
   ('k, 'a) Map \<Rightarrow> 
   ('k, 'a) Map" where
-"list_map_union_with f m1 m2 = []
+"list_map_union_with f m1 m2 = 
+  (let keys = remdups (map fst m1 @ map fst m2) in
+   map (\<lambda>k. (k, combineVals k f m1 m2)) keys
+  )
 "
+
+value "list_map_union_with (\<lambda>x y. (x @ y)) 
+          [(5 :: nat, ''a''), (3, ''b'')] 
+          [(5, ''A''), (7, ''C'')]"
+
+fun intersectVals :: "
+  'k \<Rightarrow>
+  ('k, 'a) Map \<Rightarrow> 
+  ('k, 'b) Map \<Rightarrow> 
+  'a list" where
+"intersectVals k m1 m2 =
+  (case (map_of m1 k, map_of m2 k) of 
+    (x, None) \<Rightarrow> [] |
+    (None, x) \<Rightarrow> [] |
+    (Some x, Some y) \<Rightarrow> [x]
+  )"
+
+fun list_map_intersection :: "
+  ('k, 'a) Map \<Rightarrow> 
+  ('k, 'b) Map \<Rightarrow> 
+  ('k, 'a) Map" where
+"list_map_intersection m1 m2 = 
+  (let keys = remdups (map fst m1 @ map fst m2) in
+   concat (map (\<lambda>k. map (\<lambda>x. (k, x)) (intersectVals k m1 m2)) keys)
+  )
+"
+
+value "list_map_intersection [(5 :: nat, a), (3, b)] [(5, A), (7, C)]"
 
 (*Multiplication should be swapped out for modular variant*)
 fun getEvaluate_RowCount_PowerProduct_Coefficient_Map_RowIndex_Scalar ::
@@ -414,255 +460,162 @@ fun specM :: "('a \<Rightarrow> 'b option) \<Rightarrow> 'a list \<Rightarrow> '
 
 (*Addition should be swapped out for modular variant*)
 fun getEvaluate_RowCount_Polynomial_Map_RowIndex_Scalar ::
-  "(RowCount * Polynomial, RowIndex \<rightharpoonup> Scalar) HasEvaluate" where
+  "(RowCount * Polynomial, (RowIndex, Scalar option) Map) HasEvaluate" where
 "getEvaluate_RowCount_Polynomial_Map_RowIndex_Scalar arg (rc, \<lparr> unPolynomial = monos \<rparr>) =
   (let evaluate = getEvaluate_RowCount_PowerProduct_Coefficient_Map_RowIndex_Scalar in
-  map_option (\<lambda>l . foldr (map_union_with (+)) l Map.empty)
+  map_option (\<lambda>l . mapMap Some (foldr (list_map_union_with (+)) l []))
       (specM (evaluate arg \<circ> (\<lambda>b . (rc, b))) monos)
   )"
+
+
+fun lessIndicator :: "Scalar \<Rightarrow> Scalar \<Rightarrow> Scalar" where
+"lessIndicator x y = (if x < y then 1 else 0)"
 
 fun getRowSet :: "
   RowCount \<Rightarrow>
   RowIndex set option \<Rightarrow>
-  RowIndex set" where 
-"getRowSet n None = {x. 0 \<le> x \<and> x < n}" |
-"getRowSet n (Some x) = x"
+  RowIndex set" where
+"(getRowSet n None) = of_nat ` set (upt 0 (nat n))" |
+"(getRowSet n (Some x)) = x"
 
-definition getCellMapRows :: "
-  RowIndex set \<Rightarrow>
-  (CellReference \<rightharpoonup> Scalar) \<Rightarrow>
-  RowIndex \<rightharpoonup> (ColumnIndex \<rightharpoonup> Scalar)" where
+fun getRowList :: "
+  RowCount \<Rightarrow>
+  RowIndex list option \<Rightarrow>
+  RowIndex list" where
+"(getRowList n None) = map of_nat (upt 0 (nat n))" |
+"(getRowList n (Some x)) = x"
+
+fun list_map_unions_with :: "
+  ('a \<Rightarrow> 'a \<Rightarrow> 'a) \<Rightarrow> 
+  ('k, 'a) Map list \<Rightarrow>
+  ('k, 'a) Map" where
+"list_map_unions_with f ms = foldr (list_map_union_with f) ms []"
+
+fun getCellMapRows :: "
+  (RowIndex) set \<Rightarrow>
+  (CellReference, Scalar) Map \<Rightarrow>
+  (RowIndex, (ColumnIndex, Scalar) Map) Map" where
 "getCellMapRows rows cellMap = 
-  (\<lambda>ri.
-    if ri \<in> rows 
-    then Some (\<lambda>ci. cellMap \<lparr> colIndex = ci, rowIndex = ri \<rparr>)
-    else None
-  )"
-
-(*Left-biased map intersection*)
-definition map_intersection :: "
-  ('a \<rightharpoonup> 'b) \<Rightarrow>
-  ('a \<rightharpoonup> 'c) \<Rightarrow>
-  ('a \<rightharpoonup> 'b)" where
-"map_intersection f g x =
-  (case (f x, g x) of
-    (Some a, Some _) \<Rightarrow> Some a |
-    (None, _) \<Rightarrow> None |
-    (_, None) \<Rightarrow> None
-  )"
+  list_map_unions_with (@) 
+    (map (\<lambda>(cr, x). [(rowIndex cr, [(colIndex cr, x)])])
+         (filter (\<lambda>(cr, x). rowIndex cr \<in> rows) cellMap))"
 
 fun columnListToCellMap :: "
-  ((RowIndex \<rightharpoonup> Scalar) * LookupTableColumn) list \<Rightarrow>
-  CellReference \<rightharpoonup> Scalar" where
-"columnListToCellMap cols (\<lparr> colIndex = ci, rowIndex = ri \<rparr>) = 
-  None"
+  ((RowIndex, Scalar) Map * LookupTableColumn) list \<Rightarrow>
+  (CellReference, Scalar) Map" where
+"columnListToCellMap cols = 
+  List.bind cols (\<lambda>(row, ci).
+  List.bind row (\<lambda>(ri, x).
+    [(\<lparr>colIndex=ci, rowIndex = ri\<rparr>, x)]
+  ))"
 
-(*
-Map.fromList
-    [ (CellReference ci ri, x)
-      | (row, LookupTableColumn ci) <- cols,
-        (ri, x) <- Map.toList row
-    ]
-*)
-
-definition getColumnListRows :: "
-  RowIndex set \<Rightarrow>
-  ((RowIndex \<rightharpoonup> Scalar) * LookupTableColumn) list \<Rightarrow>
-  RowIndex \<rightharpoonup> (ColumnIndex \<rightharpoonup> Scalar)" where
+fun getColumnListRows :: "
+  (RowIndex) set \<Rightarrow>
+  ((RowIndex, Scalar) Map * LookupTableColumn) list \<Rightarrow>
+  (RowIndex, (ColumnIndex, Scalar) Map) Map" where
 "getColumnListRows rows = getCellMapRows rows \<circ> columnListToCellMap"
 
-(*
-getColumnListRows ::
-  Set (RowIndex 'Absolute) ->
-  [(Map (RowIndex 'Absolute) Scalar, LookupTableColumn)] ->
-  Map (RowIndex 'Absolute) (Map ColumnIndex Scalar)
-getColumnListRows rows = getCellMapRows rows . columnListToCellMap
-*)
+fun rowsToCellMap :: "
+  (RowIndex, (ColumnIndex, Scalar) Map) Map \<Rightarrow>
+  (CellReference, Scalar) Map" where
+"rowsToCellMap rows =
+  List.bind rows (\<lambda>(ri, cols).
+  List.bind cols (\<lambda>(ci, x).
+    [(\<lparr>colIndex=ci, rowIndex=ri\<rparr>, x)]
+  ))"
 
-find_consts "('a \<rightharpoonup> 'b) \<Rightarrow> ('b \<rightharpoonup> 'a)"
+fun map_size :: "('a, 'b) Map \<Rightarrow> nat" where
+"map_size m = (length \<circ> remdups \<circ> (map fst)) m"
 
-definition getLookupResults :: "
+fun getLookupResults :: "
   RowCount \<Rightarrow>
-  RowIndex set option \<Rightarrow>
-  (CellReference \<rightharpoonup> Scalar) \<Rightarrow>
-  ((RowIndex \<rightharpoonup> Scalar) * LookupTableColumn) list \<Rightarrow>
-  (CellReference \<rightharpoonup> Scalar) option" where
-"getLookupResults rc mRowSet cellMap table = 
-  (let rowSet = getRowSet rc mRowSet in
+  RowIndex list option \<Rightarrow>
+  (CellReference, Scalar) Map \<Rightarrow>
+  ((RowIndex, Scalar) Map * LookupTableColumn) list \<Rightarrow>
+  (CellReference, Scalar) Map option" where
+"getLookupResults rc mRowSet cellMap inputTable = 
+  (let rowSet = getRowList rc mRowSet in
   (let allRows = getRowSet rc None in
-  (let cellMapAllRows = getCellMapRows allRows cellMap in
-  (let tableCols = map_of (map ((\<lambda>x. (x, ())) \<circ> snd) table) in
-  (let cellMapTableRows = map_map (\<lambda>x. map_intersection x tableCols) cellMapAllRows in
-  (let cellMapTableInverse = None in
-  (let tableRows = None in
-  None
-  )))))))"
+  (let cellMapAllRows :: (RowIndex, (ColumnIndex, Scalar) Map) Map
+        = getCellMapRows allRows cellMap in
+  (let tableCols :: (ColumnIndex, unit) Map= 
+        map ((\<lambda>x. (x, ())) \<circ> snd) inputTable in
+  (let cellMapTableRows :: (RowIndex , (ColumnIndex, Scalar) Map) Map
+        = mapMap (\<lambda>x. list_map_intersection x tableCols) cellMapAllRows in
+  (let cellMapTableInverse = map (\<lambda>(x, y). (y, x)) cellMapTableRows in
+  (let tableRows = getColumnListRows (set rowSet) inputTable in
+  map_option rowsToCellMap (those (map (\<lambda>ri. 
+    Option.bind (map_of tableRows ri) (\<lambda>inputTableRow. (
+    if (map_size inputTableRow \<noteq> length inputTable) then None
+    else (
+    case map_of cellMapTableInverse inputTableRow of
+      None \<Rightarrow> Some (ri, []) |
+      Some ri' \<Rightarrow> (
+        case map_of cellMapAllRows ri' of
+          None \<Rightarrow> None |
+          Some r \<Rightarrow> Some (ri, r)
+    ))))
+  ) rowSet)))))))))"
 
-(*
-getLookupResults ::
-  ann ->
-  RowCount ->
-  Maybe (Set (RowIndex 'Absolute)) ->
-  Map CellReference Scalar ->
-  [(Map (RowIndex 'Absolute) Scalar, LookupTableColumn)] ->
-  Either (ErrorMessage ann) (Map CellReference Scalar)
-getLookupResults ann rc mRowSet cellMap table = do
-  let rowSet = getRowSet rc mRowSet
-      allRows = getRowSet rc Nothing
-      cellMapAllRows = getCellMapRows allRows cellMap
-      tableCols = Map.fromList ((,()) . (^. #unLookupTableColumn) . snd <$> table)
-      cellMapTableRows = (`Map.intersection` tableCols) <$> cellMapAllRows
-
-      cellMapTableInverse = inverseMap cellMapTableRows
-      tableRows = getColumnListRows rowSet table
-
-  rowsToCellMap . Map.fromList
-    <$> sequence
-      [ do
-          tableRow <-
-            maybe
-              (Left (ErrorMessage ann "input table row index missing"))
-              pure
-              (Map.lookup ri tableRows)
-          when (Map.size tableRow /= length table) $
-            trace
-              ("table: " <> show table)
-              (Left (ErrorMessage ann ("table row is wrong size; duplicate column index in lookup table, or missing value in input column vectors? " <> pack (show (snd <$> table, tableRow)))))
-          case Map.lookup tableRow cellMapTableInverse of
-            Just ri' ->
-              case Map.lookup ri' cellMapAllRows of
-                Just r -> pure (ri, r)
-                Nothing ->
-                  Left
-                    ( ErrorMessage
-                        ann
-                        ("argument table row lookup failed: " <> pack (show ri'))
-                    )
-            Nothing ->
-              pure (ri, mempty) -- returning an empty row will result in no cells for this row
-        | ri <- Set.toList rowSet
-      ]
-*)
-
-definition performLookups :: "
-  (RowCount * 'a, RowIndex \<rightharpoonup> Scalar option) HasEvaluate \<Rightarrow>
+(*Note that this case is different from the haskell implementation
+  in that part of `performLookups` is computed in getEvaluate_RowCount_Term_Map_RowIndex_Scalar.
+  Specifically, is is split up and its first part is partially evaluated there.
+  This is necessary for termination checking. *)
+fun performLookups :: "
   RowCount \<Rightarrow>
   Argument \<Rightarrow>
-  ('a InputExpression * LookupTableColumn) list \<Rightarrow>
+  (RowIndex, Scalar option) Map list option \<Rightarrow>
+  LookupTableColumn list \<Rightarrow>
   LookupTableOutputColumn \<Rightarrow>
-  (RowIndex \<rightharpoonup> Scalar option) option" where
-"performLookups eval rc arg is outCol = None"
+  ((RowIndex, Scalar option) Map) option" where
+"performLookups rc arg is1 is2 c = 
+  Option.bind (map_option (map (map (\<lambda>(x, y). (x, option.the y)))) is1) 
+  (\<lambda>inputTable.
+  Option.bind (getLookupResults rc None (getCellMap arg) (zip inputTable is2))
+  (\<lambda>results.
+  (let allRows = getRowList rc None in
+  (let results' = mapMap Some (mapKeys rowIndex (map_filterWithKey (\<lambda>k. colIndex k = c) results)) in
+  Some (results' @ map (\<lambda>k. (k, None)) allRows)
+  ))))"
 
-(*
-performLookups ::
-  HasEvaluate (RowCount, a) (Map (RowIndex 'Absolute) (Maybe Scalar)) =>
-  ann ->
-  RowCount ->
-  Argument ->
-  [(InputExpression a, LookupTableColumn)] ->
-  LookupTableOutputColumn ->
-  Either (ErrorMessage ann) (Map (RowIndex 'Absolute) (Maybe Scalar))
-performLookups ann rc arg is outCol = do
-  inputTable <-
-    fmap (Map.mapMaybe id)
-      <$> mapM (evaluate ann arg . (rc,) . (^. #getInputExpression) . fst) is
-  results <-
-    getLookupResults
-      ann
-      rc
-      Nothing
-      (getCellMap arg)
-      (zip inputTable (snd <$> is))
-  let allRows = getRowSet rc Nothing
-      results' =
-        fmap Just . Map.mapKeys (^. #rowIndex) $
-          Map.filterWithKey (\k _ -> k ^. #colIndex == outCol') results
-  pure $ results' <> Map.fromList ((,Nothing) <$> Set.toList allRows)
-  where
-    outCol' = outCol ^. #unLookupTableOutputColumn . #unLookupTableColumn
-
-*)
 
 fun getEvaluate_RowCount_Term_Map_RowIndex_Scalar ::
-  "(RowCount * Term, RowIndex \<rightharpoonup> Scalar option) HasEvaluate" where
-"getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (rc, trm) =
-  (case trm of
-    Var v \<Rightarrow> map_option (map_map Some) (getEvaluate_PolynomialVariable_Map_RowIndex_Scalar arg v) | 
-    Const i \<Rightarrow> Some (\<lambda>x . (if 0 \<le> x \<and> x < rc then Some (Some i) else None)) |
-    Plus x y \<Rightarrow> 
-      ( let recx = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (rc, x) in
-      ( let recy = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (rc, y) in
-        None
-      )) |
-    Times x y \<Rightarrow> 
-      ( let recx = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (rc, x) in
-      ( let recy = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (rc, y) in
-        None
-      )) |
-    Max x y \<Rightarrow> 
-      ( let recx = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (rc, x) in
-      ( let recy = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (rc, y) in
-        None
-      )) |
-    IndLess x y \<Rightarrow> 
-      ( let recx = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (rc, x) in
-      ( let recy = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (rc, y) in
-        None
-      )) |
-    Lookup is outCol \<Rightarrow> None
-  )"
+  "(RowCount * Term, (RowIndex, Scalar option) Map) HasEvaluate" where
+"getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, Var v) =
+   map_option (mapMap Some) (getEvaluate_RowCount_PolynomialVariable_Map_RowIndex_Scalar arg (n, v))" |
+"getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, Const i) =
+   Some (map (\<lambda>x. (of_nat x, Some i)) (upt 0 (nat n)))" |
+"getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, Plus x y) =
+  (let recx :: ((RowIndex, Scalar option) Map) option = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, x) in
+  (let recy :: ((RowIndex, Scalar option) Map) option = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, y) in
+    combine_options (list_map_union_with (combine_options (+))) recx recy
+  ))" |
+"getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, Times x y) =
+  (let recx :: ((RowIndex, Scalar option) Map) option = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, x) in
+  (let recy :: ((RowIndex, Scalar option) Map) option = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, y) in
+    combine_options (list_map_union_with (combine_options (*))) recx recy
+  ))" |
+"getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, Max x y) =
+  (let recx :: ((RowIndex, Scalar option) Map) option = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, x) in
+  (let recy :: ((RowIndex, Scalar option) Map) option = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, y) in
+    combine_options (list_map_union_with (combine_options max)) recx recy
+  ))" |
+"getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, IndLess x y) =
+  (let recx :: ((RowIndex, Scalar option) Map) option = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, x) in
+  (let recy :: ((RowIndex, Scalar option) Map) option = getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, y) in
+    combine_options (list_map_union_with (combine_options lessIndicator)) recx recy
+  ))" |
+(*Note that this case is different from the haskell implementation
+  in that part of `performLookups` is computed here. This is
+  necessary for termination checking.*)
+"getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, Lookup is outCol) =
+  (let is1 :: (RowIndex, Scalar option) Map list option =
+       those (map (\<lambda>x. getEvaluate_RowCount_Term_Map_RowIndex_Scalar arg (n, fst x)) is) in
+  (let is2 = map snd is in
+  performLookups n arg is1 is2 outCol
+  ))"
 
 
-(*
-instance HasEvaluate (RowCount, Term) (Map (RowIndex 'Absolute) (Maybe Scalar)) where
-  evaluate ann arg = uncurry $ \rc@(RowCount n) ->
-    let rec = evaluate ann arg . (rc,)
-     in \case
-          Plus x y -> Map.unionWith (liftA2 (Group.+)) <$> rec x <*> rec y
-          Times x y -> Map.unionWith (liftA2 (Ring.* )) <$> rec x <*> rec y
-          Max x y -> Map.unionWith (liftA2 max) <$> rec x <*> rec y
-          IndLess x y -> Map.unionWith (liftA2 lessIndicator) <$> rec x <*> rec y
-          l@(Lookup is outCol) ->
-            mapLeft
-              ( \(ErrorMessage ann' msg) ->
-                  ErrorMessage ann' ("performLookups " <> pack (show l) <> ": " <> msg)
-              )
-              (performLookups ann rc arg is outCol)
-*)
-
-
-
-
-
-
-(*
-performLookups ::
-  HasEvaluate (RowCount, a) (Map (RowIndex 'Absolute) (Maybe Scalar)) =>
-  ann ->
-  RowCount ->
-  Argument ->
-  [(InputExpression a, LookupTableColumn)] ->
-  LookupTableOutputColumn ->
-  Either (ErrorMessage ann) (Map (RowIndex 'Absolute) (Maybe Scalar))
-performLookups ann rc arg is outCol = do
-  inputTable <-
-    fmap (Map.mapMaybe id)
-      <$> mapM (evaluate ann arg . (rc,) . (^. #getInputExpression) . fst) is
-  results <-
-    getLookupResults
-      ann
-      rc
-      Nothing
-      (getCellMap arg)
-      (zip inputTable (snd <$> is))
-  let allRows = getRowSet rc Nothing
-      results' =
-        fmap Just . Map.mapKeys (^. #rowIndex) $
-          Map.filterWithKey (\k _ -> k ^. #colIndex == outCol') results
-  pure $ results' <> Map.fromList ((,Nothing) <$> Set.toList allRows)
-  where
-    outCol' = outCol ^. #unLookupTableOutputColumn . #unLookupTableColumn
-*)
 
 
 end
